6eca3571b4c62a0e6c9a838e0493ae9ac323e20f
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-09-30 19:21:17 -0500

    Add files via upload

---
+++ LAB_1.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+#include "string"
+#include <stdio.h>
+
+using namespace std;
+
+struct node{
+    int coef;
+    int grado;
+    node*next;
+};
+struct polinomio{
+    node*head=NULL;
+    void insert(int coef, int grado){
+        node* new__node=new node();
+        new__node-> coef = coef;
+        new__node-> grado= grado;
+    }
+};
+
+void print(){
+    node * x = this->  head;
+    while(x!=NULL){
+        cout<<x->coef<<"x"<<x->grado;
+        x=x->next;
+    }
+    cout<<"";
+}
+
