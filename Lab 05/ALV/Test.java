8badf9a548c8473e956555b7ef7a6ee2a8c215e3
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-12-09 04:15:24 -0500

    Update Test.java

--- Lab 05/ALV/Test.java
+++ Lab 05/ALV/Test.java
@@ -1,1 +1,12 @@
+public class Test{
+	public static void main(String[] args){
+		AVL<Integer> arbol = new AVL<Integer>();
+		arbol.add(11);
+		System.out.println(arbol.inOrder());
+		arbol.add(12);
+		System.out.println(arbol.inOrder());
+		arbol.add(14);
+		System.out.println(arbol.inOrder());
+	}
+}
 
