d8bfef6c64e384fae9bf709f48d49f3dce746821
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-12-09 04:14:34 -0500

    Update Node.java

--- Lab 05/ALV/Node.java
+++ Lab 05/ALV/Node.java
@@ -1,1 +1,40 @@
+public class Node<T extends Comparable<T>>{
+	private T data;
+	private Node<T> right;
+	private Node<T> left;
+
+	public Node(T data){
+		this.data = data;
+		this.right = null;
+		this.left = null;
+	}
+	public Node(T data, Node<T> right, Node<T> left){
+		this.data = data;
+		this.right = right;
+		this.left = left;
+	}
+
+	public Node<T> getRight(){
+		return this.right;
+	}
+	public Node<T> getLeft(){
+		return this.left;
+	}
+	public void setRight(Node<T> nodo){
+		this.right = nodo;
+	}
+	public void setLeft(Node<T> nodo){
+		this.left = nodo;
+	}
+	public T getData(){
+		return this.data;
+	}
+	public void setData(T data){
+		this.data = data;
+	}
+
+	public String toString(){
+		return data.toString();
+	}
+}
 
