8ac110d1e111de3b99a5812c594cbe49b47e7880
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-12-15 23:47:42 -0500

    Update HASHING.cpp

--- LAB07/HASHING.cpp
+++ LAB07/HASHING.cpp
@@ -1,1 +1,355 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define CAPACITY 50000 // Tamaño de la tabla hash
+
+unsigned long hash_function(char* str) {
+    unsigned long i = 0;
+    for (int j=0; str[j]; j++)
+        i += str[j];
+    return i % CAPACITY;
+}
+
+typedef struct Ht_item Ht_item;
+
+// Definir el elemento de la tabla hash aquí
+struct Ht_item {
+    char* key;
+    char* value;
+};
+
+
+typedef struct LinkedList LinkedList;
+
+// Definir la lista enlazada aquí
+struct LinkedList {
+    Ht_item* item; 
+    LinkedList* next;
+};
+
+
+typedef struct HashTable HashTable;
+
+// Definir la tabla hash aquí
+struct HashTable {
+    // Contiene una matriz de punteros
+     // a los elementos
+    Ht_item** items;
+    LinkedList** overflow_buckets;
+    int size;
+    int count;
+};
+
+
+static LinkedList* allocate_list () {
+    // Asigna memoria para un puntero de lista enlazada
+    LinkedList* list = (LinkedList*) malloc (sizeof(LinkedList));
+    return list;
+}
+
+static LinkedList* linkedlist_insert(LinkedList* list, Ht_item* item) {
+    // Inserta el elemento en la lista enlazada
+    if (!list) {
+        LinkedList* head = allocate_list();
+        head->item = item;
+        head->next = NULL;
+        list = head;
+        return list;
+    } 
+    
+    else if (list->next == NULL) {
+        LinkedList* node = allocate_list();
+        node->item = item;
+        node->next = NULL;
+        list->next = node;
+        return list;
+    }
+
+    LinkedList* temp = list;
+    while (temp->next->next) {
+        temp = temp->next;
+    }
+    
+    LinkedList* node = allocate_list();
+    node->item = item;
+    node->next = NULL;
+    temp->next = node;
+    
+    return list;
+}
+
+static Ht_item* linkedlist_remove(LinkedList* list) {
+    if (!list)
+        return NULL;
+    if (!list->next)
+        return NULL;
+    LinkedList* node = list->next;
+    LinkedList* temp = list;
+    temp->next = NULL;
+    list = node;
+    Ht_item* it = NULL;
+    memcpy(temp->item, it, sizeof(Ht_item));
+    free(temp->item->key);
+    free(temp->item->value);
+    free(temp->item);
+    free(temp);
+    return it;
+}
+
+static void free_linkedlist(LinkedList* list) {
+    LinkedList* temp = list;
+    while (list) {
+        temp = list;
+        list = list->next;
+        free(temp->item->key);
+        free(temp->item->value);
+        free(temp->item);
+        free(temp);
+    }
+}
+
+static LinkedList** create_overflow_buckets(HashTable* table) {
+    LinkedList** buckets = (LinkedList**) calloc (table->size, sizeof(LinkedList*));
+    for (int i=0; i<table->size; i++)
+        buckets[i] = NULL;
+    return buckets;
+}
+
+static void free_overflow_buckets(HashTable* table) {
+    LinkedList** buckets = table->overflow_buckets;
+    for (int i=0; i<table->size; i++)
+        free_linkedlist(buckets[i]);
+    free(buckets);
+}
+
+
+Ht_item* create_item(char* key, char* value) {
+   // Crea un puntero a un nuevo elemento de tabla hash
+    Ht_item* item = (Ht_item*) malloc (sizeof(Ht_item));
+    item->key = (char*) malloc (strlen(key) + 1);
+    item->value = (char*) malloc (strlen(value) + 1);
+    
+    strcpy(item->key, key);
+    strcpy(item->value, value);
+
+    return item;
+}
+
+HashTable* create_table(int size) {
+    // Crea una nueva tabla hash
+    HashTable* table = (HashTable*) malloc (sizeof(HashTable));
+    table->size = size;
+    table->count = 0;
+    table->items = (Ht_item**) calloc (table->size, sizeof(Ht_item*));
+    for (int i=0; i<table->size; i++)
+        table->items[i] = NULL;
+    table->overflow_buckets = create_overflow_buckets(table);
+
+    return table;
+}
+
+void free_item(Ht_item* item) {
+    // Libera un elemento
+    free(item->key);
+    free(item->value);
+    free(item);
+}
+
+void free_table(HashTable* table) {
+    for (int i=0; i<table->size; i++) {
+        Ht_item* item = table->items[i];
+        if (item != NULL)
+            free_item(item);
+    }
+
+    free_overflow_buckets(table);
+    free(table->items);
+    free(table);
+}
+
+void handle_collision(HashTable* table, unsigned long index, Ht_item* item) {
+    LinkedList* head = table->overflow_buckets[index];
+
+    if (head == NULL) {
+       // Necesitamos crear la lista
+        head = allocate_list();
+        head->item = item;
+        table->overflow_buckets[index] = head;
+        return;
+    }
+    else {
+        // Insertar en la lista
+        table->overflow_buckets[index] = linkedlist_insert(head, item);
+        return;
+    }
+ }
+
+void ht_insert(HashTable* table, char* key, char* value) {
+    // Crea el elemento
+    Ht_item* item = create_item(key, value);
+
+    // Calcular el índice
+    unsigned long index = hash_function(key);
+
+    Ht_item* current_item = table->items[index];
+    
+    if (current_item == NULL) {
+        // La clave no existe.
+        if (table->count == table->size) {
+            // Tabla hash llena
+            printf("Insert Error: Hash Table is full\n");
+            // Eliminar el elemento de creado
+            free_item(item);
+            return;
+        }
+        
+        // Insertar directamente
+        table->items[index] = item; 
+        table->count++;
+    }
+
+    else {
+            // Escenario 1: Solo necesitamos actualizar el valor
+            if (strcmp(current_item->key, key) == 0) {
+                strcpy(table->items[index]->value, value);
+                return;
+            }
+    
+        else {
+            // Escenario 2: Colisión
+            handle_collision(table, index, item);
+            return;
+        }
+    }
+}
+
+char* ht_search(HashTable* table, char* key) {
+	 // Busca la clave en la tabla hash
+     // y devuelve NULL si no existe
+    int index = hash_function(key);
+    Ht_item* item = table->items[index];
+    LinkedList* head = table->overflow_buckets[index];
+
+    // Asegurarnos de que nos movemos a elementos que no son NULL
+    while (item != NULL) {
+        if (strcmp(item->key, key) == 0)
+            return item->value;
+        if (head == NULL)
+            return NULL;
+        item = head->item;
+        head = head->next;
+    }
+    return NULL;
+}
+
+void ht_delete(HashTable* table, char* key) {
+    // Elimina un elemento de la tabla
+    int index = hash_function(key);
+    Ht_item* item = table->items[index];
+    LinkedList* head = table->overflow_buckets[index];
+
+    if (item == NULL) {
+        // No existe. retorno
+        return;
+    }
+    else {
+        if (head == NULL && strcmp(item->key, key) == 0) {
+            // Sin cadena de colisión. Quitar el artículo
+			// y establece el índice de la tabla en NULL
+            table->items[index] = NULL;
+            free_item(item);
+            table->count--;
+            return;
+        }
+        else if (head != NULL) {
+            // si la cadena de colisión existe
+            if (strcmp(item->key, key) == 0) {
+                // Eliminar este elemento y establecer el encabezado de la lista
+				// como el nuevo elemento
+                
+                free_item(item);
+                LinkedList* node = head;
+                head = head->next;
+                node->next = NULL;
+                table->items[index] = create_item(node->item->key, node->item->value);
+                free_linkedlist(node);
+                table->overflow_buckets[index] = head;
+                return;
+            }
+
+            LinkedList* curr = head;
+            LinkedList* prev = NULL;
+            
+            while (curr) {
+                if (strcmp(curr->item->key, key) == 0) {
+                    if (prev == NULL) {
+                        // Primer elemento de la cadena. quitar la cadena
+                        free_linkedlist(head);
+                        table->overflow_buckets[index] = NULL;
+                        return;
+                    }
+                    else {
+                        prev->next = curr->next;
+                        curr->next = NULL;
+                        free_linkedlist(curr);
+                        table->overflow_buckets[index] = head;
+                        return;
+                    }
+                }
+                curr = curr->next;
+                prev = curr;
+            }
+
+        }
+    }
+}
+
+void print_search(HashTable* table, char* key) {
+    char* val;
+    if ((val = ht_search(table, key)) == NULL) {
+        printf("%s NULL\n", key);
+        return;
+    }
+    else {
+        printf("Clave:%s, Valor:%s\n", key, val);
+    }
+}
+
+void print_table(HashTable* table) {
+    printf("\n-------------------\n");
+    for (int i=0; i<table->size; i++) {
+        if (table->items[i]) {
+            printf("Indice:%d, Clave:%s, Valor:%s", i, table->items[i]->key, table->items[i]->value);
+            if (table->overflow_buckets[i]) {
+                printf(" => desbordamiento => ");
+                LinkedList* head = table->overflow_buckets[i];
+                while (head) {
+                    printf("Clave:%s, Valor:%s ", head->item->key, head->item->value);
+                    head = head->next;
+                }
+            }
+            printf("\n");
+        }
+    }
+    printf("-------------------\n");
+}
+
+int main() {
+    HashTable* ht = create_table(CAPACITY);
+    ht_insert(ht, "1", "Primera direccion");
+    ht_insert(ht, "2", "Segunda direccion");
+    ht_insert(ht, "Hel", "Tercera direccion");
+    ht_insert(ht, "Cau", "Cuarta direccion");
+    print_search(ht, "1");
+    print_search(ht, "2");
+    print_search(ht, "3");
+    print_search(ht, "Hel");
+    print_search(ht, "Cau");  // Colision
+    print_table(ht);
+    ht_delete(ht, "1");
+    ht_delete(ht, "Cau");
+    print_table(ht);
+    free_table(ht);
+    return 0;
+}
