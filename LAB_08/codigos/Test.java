12f6c710d6400ef7acb0ff022c9f8775c255573d
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-12-18 22:59:27 -0500

    Add files via upload

---
+++ LAB_08/codigos/Test.java
@@ -0,0 +1,23 @@
+package Ejercicio2;
+
+import java.io.IOException;
+
+public class Test {
+	public static void main(String[] args) throws Exception  {
+		//Prueba de algoritmo dijtra
+//		GrafoNDP grafo3 = new GrafoNDP("C:\\Users\\EDISSON FRANKLIN\\Desktop\\Entradas\\dijkstra\\grafo.in");
+//		Dijkstra dijkstra = new Dijkstra(grafo3, 0);
+//		dijkstra.ejecutar();
+		
+		
+//		//Prueba de Algoritmo DFS
+//		GrafoNDNP grafo2 = new GrafoNDNP("C:\\Users\\EDISSON FRANKLIN\\Desktop\\Entradas\\BFS\\grafo.in");
+//		DFS dfs = new DFS(grafo2, 4);
+//		dfs.ejecutar();
+		
+		//Prueba de algoritmo BFS
+		GrafoNDNP grafo1 = new GrafoNDNP("C:\\Users\\EDISSON FRANKLIN\\Desktop\\Entradas\\BFS\\grafo.in");
+		BFS bfs = new BFS(grafo1, 4);
+		bfs.ejecutar();
+		}
+}
