2ebb36a5bd90df14a870ef004ea455bdbf8af904
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2022-12-18 23:07:13 -0500

    Update Test.java

--- LAB_08/codigos/Test.java
+++ LAB_08/codigos/Test.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 public class Test {
 	public static void main(String[] args) throws Exception  {
 		//Prueba de algoritmo dijtra
-//		GrafoNDP grafo3 = new GrafoNDP("C:\\Users\\EDISSON FRANKLIN\\Desktop\\Entradas\\dijkstra\\grafo.in");
+//		GrafoNDP grafo3 = new GrafoNDP("C:\\Users\\hp\\Desktop\\Entradas\\dijkstra\\grafo.in");
 //		Dijkstra dijkstra = new Dijkstra(grafo3, 0);
 //		dijkstra.ejecutar();
 		
 		
 //		//Prueba de Algoritmo DFS
-//		GrafoNDNP grafo2 = new GrafoNDNP("C:\\Users\\EDISSON FRANKLIN\\Desktop\\Entradas\\BFS\\grafo.in");
+//		GrafoNDNP grafo2 = new GrafoNDNP("C:\\Users\\hp\\Desktop\\Entradas\\BFS\\grafo.in");
 //		DFS dfs = new DFS(grafo2, 4);
 //		dfs.ejecutar();
 		
 		//Prueba de algoritmo BFS
-		GrafoNDNP grafo1 = new GrafoNDNP("C:\\Users\\EDISSON FRANKLIN\\Desktop\\Entradas\\BFS\\grafo.in");
-		BFS bfs = new BFS(grafo1, 4);
-		bfs.ejecutar();
+//		GrafoNDNP grafo1 = new GrafoNDNP("C:\\Users\\hp\\Desktop\\Entradas\\BFS\\grafo.in");
+//		BFS bfs = new BFS(grafo1, 4);
+//		bfs.ejecutar();
 		}
 }
